d74c1dc96ff70dec5810d10af4cb7f8fdf7e5e05
tx4x <baumgart.guenter02@gmail.com>
2019-02-13 01:03:48 +0100

    Delete build-agent.sh

--- scripts/build-agent.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ./dist/modules/Q_sessionrecorder
-rm -rf ./dist/modules/D_sessionrecorderworker
-rm -rf ./dist/modules/A_asyncCore
-rm -rf ./dist/modules/c_core
-rm -rf ./dist/modules/C_initCode
-( yarn build --target=onearmy && yarn update-onearmy-server ) || ( echo "FAIL" && read )
